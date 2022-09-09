e95e95ffac8915b7ab23485218be63867308cdd1
Aiyan-Faras <farasaiyan@gmail.com>
2022-09-09 15:26:45 +0530

    first commit

--- filemaker.js
+++ filemaker.js
@@ -1,10 +1,2 @@
 const fs = require('fs');
 
-try { 
-    fs.mkdirSync('Myproject');
-    fs.writeFileSync('Myprojlect/index.html');
-    fs.writeFileSync('Myproject/app.js');
-    fs.writeFileSync('Myproject/style.css');
-} catch (err) { 
-    console.log("something went wrong")
-}
