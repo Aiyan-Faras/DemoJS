75d341dba79f62073a35e7c508861e31e3bfa856
Aiyan-Faras <farasaiyan@gmail.com>
2022-09-09 15:28:47 +0530

    used fs module

--- filemaker.js
+++ filemaker.js
@@ -1,2 +1,10 @@
 const fs = require('fs');
 
+try { 
+    fs.mkdirSync('Myproject');
+    fs.writeFileSync('Myprojlect/index.html');
+    fs.writeFileSync('Myproject/app.js');
+    fs.writeFileSync('Myproject/style.css');
+} catch (err) { 
+    console.log("something went wrong")
+}
