7af4de12b8c187f78a3e88f9da0bb8ce7bdc069f
Aiyan-Faras <farasaiyan@gmail.com>
2022-09-27 20:04:18 +0530

    committed

--- newStart/index.js
+++ newStart/index.js
@@ -5,7 +5,16 @@ app.listen(3000, () => (
     console.log("listening to port 3000")
 ))
 
-app.use(() => {
-    ++count
-    console.log("visited " + count)
+// app.use(() => {
+//     ++count
+//     console.log("visited " + count)
+// })
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome this is my index page</h1>`)
+})
+
+app.get('/:anything/:more', (req, res) => {
+    const { anything , more} = req.params;
+    res.send(`<h1>welcome to ${anything} and ${more} page<h1>`)
 })
