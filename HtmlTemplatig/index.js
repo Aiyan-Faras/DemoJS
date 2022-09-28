600387efd6901d834b34a9eca929d1cb64c4e566
Aiyan-Faras <farasaiyan@gmail.com>
2022-09-28 22:56:55 +0530

    express routing

--- HtmlTemplatig/index.js
+++ HtmlTemplatig/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const data = require('./data.json')
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+	const rand = Math.floor(Math.random()* 10);
+	res.render('index', { random : rand});
+})
+
+app.get('/:subreddit', (req, res) => {
+	const { subreddit } = req.params;
+	const type = data[subreddit];
+	res.render('chicken', {...type } )
+ })
 
 app.listen(3000,(req, res) =>{
 	console.log("Hey there")
